305a518f73dba67a26ff2c18d6696468809e01b5
David Oyewale <davidwale2003@gmail.com>
2025-04-10 16:15:55 +0100

    make auth screens responsive

--- src/components/auth-left-Section.jsx
+++ src/components/auth-left-Section.jsx
@@ -3,7 +3,7 @@ const AuthLeftSection = () => {
 
 
   return (
-   <div>
+   <div className="hidden md:flex">
     <img  className=" max-h-screen w-[400px]" src="/assets/auth-left-section.png" alt="auth left section" />
    </div>
   )
